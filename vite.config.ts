21b0a956355eac4b20d571582137af6f5f268d37
mixup <l21170458@gmail.com>
2025-11-18 23:09:27 -0700

    Corrección de motor de renderizado de vistas

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
-import { wayfinder } from '@laravel/vite-plugin-wayfinder';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
+import {
+    defineConfig
+} from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.tsx',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        react(),
         tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
     ],
-    esbuild: {
-        jsx: 'automatic',
+    server: {
+        cors: true,
     },
 });
